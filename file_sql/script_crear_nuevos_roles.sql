badfa325d63d1625884a857a6857ae155d2f0841
Witman Jose Gonzalez Rostran <wgonzalez@cgclatam.com>
2022-11-01 21:55:54 -0600

    listo

--- file_sql/script_crear_nuevos_roles.sql
+++ file_sql/script_crear_nuevos_roles.sql
@@ -8,7 +8,7 @@ insert into tb_role(
 select 	
 	companyID,branchID,
 	REPLACE(cc.`name`,@nameCompanyOrigen,@nameCompany) as `name`,description,
-	createdOn,urlDefault,isAdmin,createdBy ,isActive
+	createdOn,'core_dashboards' as urlDefault,0 as isAdmin,createdBy ,isActive
 from 
 	tb_role cc 
 where
@@ -29,6 +29,21 @@ where
 	r.`name` = CONCAT(@nameCompanyOrigen,'@ADMINISTRADOR');
 
 
+set @roleAdmin := (select u.roleID from tb_role u where u.`name` = CONCAT(@nameCompany,'@ADMINISTRADOR') );
+insert into tb_role_autorization(companyID,componentAutorizationID,roleID,branchID)
+select 
+	 e.companyID,
+	 componentAutorizationID,
+	 @roleAdmin,
+	 e.branchID
+from 
+	tb_role_autorization e
+	inner join tb_role r on 
+		e.roleID = r.roleID 
+where
+	r.`name` = CONCAT(@nameCompanyOrigen,'@ADMINISTRADOR');
+
+
 #insertar permisos del rol facturador
 set @roleFacturador := (select u.roleID from tb_role u where u.`name` = CONCAT(@nameCompany,'@FACTURADOR') );
 insert into tb_user_permission(elementID,companyID,branchID,roleID,selected,inserted,deleted,edited)
@@ -41,6 +56,19 @@ from
 where
 	r.`name` = CONCAT(@nameCompanyOrigen,'@FACTURADOR');
 
+set @roleFacturador := (select u.roleID from tb_role u where u.`name` = CONCAT(@nameCompany,'@FACTURADOR') );
+insert into tb_role_autorization(companyID,componentAutorizationID,roleID,branchID)
+select 
+	 e.companyID,
+	 componentAutorizationID,
+	 @roleFacturador,
+	 e.branchID
+from 
+	tb_role_autorization e
+	inner join tb_role r on 
+		e.roleID = r.roleID 
+where
+	r.`name` = CONCAT(@nameCompanyOrigen,'@FACTURADOR');
 
 #crear el rol de supervisor
 set @roleSupervisor := (select u.roleID from tb_role u where u.`name` = CONCAT(@nameCompany,'@SUPERVISOR') );
@@ -49,6 +77,21 @@ select
 	e.elementID,e.companyID,e.branchID,@roleSupervisor roleID,e.selected,e.inserted,e.deleted,e.edited
 from 
 	tb_user_permission e
+	inner join tb_role r on 
+		e.roleID = r.roleID 
+where
+	r.`name` = CONCAT(@nameCompanyOrigen,'@SUPERVISOR');
+
+
+set @roleSupervisor := (select u.roleID from tb_role u where u.`name` = CONCAT(@nameCompany,'@SUPERVISOR') );
+insert into tb_role_autorization(companyID,componentAutorizationID,roleID,branchID)
+select 
+	 e.companyID,
+	 componentAutorizationID,
+	 @roleSupervisor,
+	 e.branchID
+from 
+	tb_role_autorization e
 	inner join tb_role r on 
 		e.roleID = r.roleID 
 where
