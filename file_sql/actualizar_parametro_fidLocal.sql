1756aa1a1b598fe1760c20b11eb55e2916f17c25
Witman Jose Gonzalez Rostran <wgonzalez@cgclatam.com>
2022-11-25 20:54:51 -0600

    listo

--- file_sql/actualizar_parametro_fidLocal.sql
+++ file_sql/actualizar_parametro_fidLocal.sql
@@ -293,7 +293,7 @@ WHERE
 				
 				
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "flc_witman" 
+	tb_company_parameter.value = "flc-wgonzalez" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "CORE_CXC_WSDL_SIN_RIESGO_USUARIO";## Usuario para conectarse a la sin riesgo 
