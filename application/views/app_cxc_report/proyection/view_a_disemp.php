e8a3744b9d23e509010df20eb12d639f0a33b45c
Witman Jose Gonzalez Rostran <wgonzalez@cgclatam.com>
2023-01-09 08:54:14 -0600

    formato a los reports

--- application/views/app_cxc_report/proyection/view_a_disemp.php
+++ application/views/app_cxc_report/proyection/view_a_disemp.php
@@ -6,93 +6,94 @@
 		<meta name="viewport" 			content="width=device-width, initial-scale=1.0">
 		<meta name="application-name" 	content="dsemp" /> 
 		
-		<link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>css/style_table_report_printer.css">
-		<link rel="stylesheet" type="text/css" href="<?php echo site_url(); ?>css/style_table_report_printer.css" media="print">
+		<?php 
+		echo helper_reporteGeneralCreateStyle();
+		?>
 		
 	</head>
-	<body> 
-		<div class="data_grid_encabezado">
-			<table>
-				<thead>
-					<tr>
-						<th  colspan="9">PROYECCION</th>
-					</tr>
-					<tr>
-						<th  colspan="9"><?php echo strtoupper($objCompany->name); ?></th>
-					</tr>
-					<tr>
-						<th  colspan="9">LISTA DE CUENTAS</th>
-					</tr>
-				</thead>
-			</table>
-		</div>
+	<body style="font-family:monospace;font-size:smaller;margin:0px 0px 0px 0px"> 
 		
-		<br/>
-		<div class="data_grid_body">
-			<table>
-				<thead>
-					<tr>
-						<th nowrap class="cell_left">Codigo</th>
-						<th nowrap class="cell_left">Cliente</th>						
-						<th nowrap class="cell_left">Fecha</th>
-						<th nowrap class="cell_left">Periodo</th>
-						<th nowrap class="cell_left">Moneda</th>
-						<th nowrap class="cell_right">Capital</th>
-						<th nowrap class="cell_right">Interest</th>
-						<th nowrap class="cell_right">Cuota</th>
-						<th nowrap class="cell_right">Restante</th>						
-					</tr>
-				</thead>				
-				<tbody>
-					<?php
-					$count 		= 0;
-					if($objDetail)
-					foreach($objDetail as $i){
-						$count++;						
-						echo "<tr>";
-							echo "<td nowrap class='cell_left'>";
-								echo ($i["customerNumber"]);
-							echo "</td>";
-							echo "<td nowrap class='cell_left'>";
-								echo ($i["legalName"]);
-							echo "</td>";						
-							echo "<td nowrap class='cell_left'>";
-								echo ($i["Fecha"]);
-							echo "</td>";
-							echo "<td nowrap class='cell_left'>";
-								echo ($i["FechaPeriodo"]);
-							echo "</td>";
-							echo "<td nowrap class='cell_left'>";
-								echo ($i["Moneda"]);
-							echo "</td>";
-							echo "<td nowrap class='cell_right'>";
-								echo sprintf("%01.2f",$i["capital"]);
-							echo "</td>";
-							echo "<td nowrap class='cell_right'>";
-								echo sprintf("%01.2f",$i["interest"]);
-							echo "</td>";
-							echo "<td nowrap class='cell_right'>";
-								echo sprintf("%01.2f",$i["cuota"]);
-							echo "</td>";
-							echo "<td nowrap class='cell_right'>";
-								echo sprintf("%01.2f",$i["remaining"]);
-							echo "</td>";
-						echo "</tr>";
-					}
-					?>
-				</tbody>
-			</table>
-		</div>
-		<br/>
-		<div class="data_grid_firm_system">
-			<table>
-				<tbody>
-					<tr>
-						<td colspan="9"><?php echo date("Y-m-d H:i:s");  ?> <?php echo $objFirmaEncription; ?></td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+		
+		<?php
+		$configColumn["0"]["Titulo"] 		= "Codigo";		
+		$configColumn["1"]["Titulo"] 		= "Cliente";		
+		$configColumn["2"]["Titulo"] 		= "Fecha";		
+		$configColumn["3"]["Titulo"] 		= "Periodo";		
+		$configColumn["4"]["Titulo"] 		= "Moneda";		
+		$configColumn["5"]["Titulo"] 		= "Capital";		
+		$configColumn["6"]["Titulo"] 		= "Interes";		
+		$configColumn["7"]["Titulo"] 		= "Cuota";		
+		$configColumn["8"]["Titulo"] 		= "Restante";
+		
+		$configColumn["0"]["FiledSouce"] 		= "customerNumber";		
+		$configColumn["1"]["FiledSouce"] 		= "legalName";		
+		$configColumn["2"]["FiledSouce"] 		= "Fecha";		
+		$configColumn["3"]["FiledSouce"] 		= "FechaPeriodo";		
+		$configColumn["4"]["FiledSouce"] 		= "Moneda";		
+		$configColumn["5"]["FiledSouce"] 		= "capital";		
+		$configColumn["6"]["FiledSouce"] 		= "interest";		
+		$configColumn["7"]["FiledSouce"] 		= "cuota";		
+		$configColumn["8"]["FiledSouce"] 		= "remaining";
+		
+		$configColumn["0"]["Width"] 		= "80px";		
+		$configColumn["1"]["Width"] 		= "220px";		
+		$configColumn["2"]["Width"] 		= "80px";		
+		$configColumn["3"]["Width"] 		= "80px";		
+		$configColumn["4"]["Width"] 		= "80px";		
+		$configColumn["5"]["Width"] 		= "80px";		
+		$configColumn["6"]["Width"] 		= "80px";		
+		$configColumn["7"]["Width"] 		= "80px";		
+		$configColumn["8"]["Width"] 		= "80px";
+		
+		
+		$configColumn["2"]["Formato"] 		= "Date";		
+		$configColumn["5"]["Formato"] 		= "Number";		
+		$configColumn["6"]["Formato"] 		= "Number";		
+		$configColumn["7"]["Formato"] 		= "Number";		
+		$configColumn["8"]["Formato"] 		= "Number";
+		
+			
+		$configColumn["5"]["Total"] 		= "True";		
+		$configColumn["6"]["Total"] 		= "True";		
+		$configColumn["7"]["Total"] 		= "True";		
+		$configColumn["8"]["Total"] 		= "True";
+		
+		
+		
+		
+		
+		$resultado = helper_reporteGeneralCreateTable($objDetail,$configColumn,'0');
+		?>
+		
+		
+		<?php 
+		echo helper_reporteGeneralCreateEncabezado(
+			'PROYECCION',
+			$objCompany->name,
+			$resultado["columnas"],
+			'LISTA DE CUENTAS',
+			"",
+			"",
+			$resultado["width"]
+		);
+		?>
+		
+		<br/>	
+		
+		<?php 
+		echo $resultado["table"];
+		?>
+		
+		<br/>		
+		<?php 
+		echo helper_reporteGeneralCreateFirma(	
+			$objFirmaEncription,
+			$resultado["columnas"],
+			$resultado["width"]
+		);
+		?>
+		
+		
 		
 	</body>	
 </html>
